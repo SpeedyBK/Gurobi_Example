b6d438e984137102f061e5cc48ba211d6660c1d6
Benjamin Kessler <bkessler@animal.digi.e-technik.uni-kassel.de>
2024-03-21 08:25:16 +0100

    first commit

---
+++ CMakeFiles/GurobiExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GurobiExample.dir/main.cpp.o"
+  "CMakeFiles/GurobiExample.dir/main.cpp.o.d"
+  "GurobiExample"
+  "GurobiExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GurobiExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
